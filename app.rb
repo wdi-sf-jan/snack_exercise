cafdd69ba44a54d8368a4430cb38b1ec9538d6d4
Tim Licata <tim@timothylicata.com>
2015-02-18 15:35:42 -0800

    add CRUD functionality for snacks

--- app.rb
+++ app.rb
@@ -7,5 +7,39 @@ get '/' do
 end
 
 get '/snacks' do
-  "Hello, Snacks!"
+  @snacks = Snack.all
+  erb :index
+end
+
+get '/snacks/new' do
+  erb :new
+end
+
+get '/snacks/:id' do
+  @snack = Snack.find(params[:id])
+  erb :show
+end
+
+get '/snacks/:id/edit' do
+  @snack = Snack.find(params[:id])
+  erb :edit
+end
+
+post '/snacks' do
+  Snack.create({:name => params[:snack_name]})
+  redirect '/snacks'
+end
+
+put '/snacks/:id' do
+  snack = Snack.find(params[:id])
+  snack.name = params[:snack_name]
+  snack.save
+  # could have also used update()
+  redirect '/snacks'
+end
+
+delete '/snacks/:id' do
+  snack = Snack.find(params[:id])
+  snack.destroy
+  redirect '/snacks'
 end
